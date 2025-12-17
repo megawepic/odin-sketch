fd14b4f0e888ba70175a1113a0c8877168808057
megawepic <efps10.5pyapdarren@gmail.com>
2025-12-17 19:17:59 +0800

    edit index.html and code.js

--- code.js
+++ code.js
@@ -1,25 +1,54 @@
 const grid = document.querySelector(".grid")
 
 let mouseDown = false;
+let size = 16
+createGrid(size)
+
+function createGrid(size){
+    for (let i = 0; i < size*size; i++){
+        const square = document.createElement("div");
+
+        square.addEventListener("mousedown", paintSquare);
+        square.addEventListener("mouseenter", paintSquare);
+        square.style.flex = `0 0 ${100 / size}%`;
+        square.classList.add("square");
+
+        grid.append(square)
+    }
+}
 
 document.body.addEventListener("mousedown", () => mouseDown = true);
-document.body.addEventListener("mouseup", () => mouseDown = false);4
-
-for (let i = 0; i < 30*30; i++){
-    const square = document.createElement("div");
-
-
-
-    square.addEventListener("mousedown", paintSquare);
-    square.addEventListener("mouseenter", paintSquare);
-    square.style.flex = `0 0 ${100 / 30}%`;
-    square.classList.add("square");
-
-    grid.append(square)
-}
+document.body.addEventListener("mouseup", () => mouseDown = false);
 
 function paintSquare(e) {
     if (e.type === "mousedown" || mouseDown) {
         this.style.backgroundColor = "black";
     }
+}
+
+const reset = document.querySelector(".reset")
+reset.addEventListener("click", resetGrid)
+
+function resetGrid(){
+    const squares = document.querySelectorAll(".square")
+
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    });
+}
+
+const change = document.querySelector(".change-grid")
+change.addEventListener("click", changeGrid)
+
+function changeGrid() {
+    const userInput = parseInt(document.getElementById("userInput").value)
+
+    if (isNaN(userInput) || userInput < 1 || userInput > 100) {
+        alert("Please enter a valid number between 1 and 100");
+        return;
+    }
+    
+    grid.innerHTML = "";
+    
+    createGrid(userInput)
 }
