378671b350b8e876fef8a4f89fe91335fe20e18f
megawepic <efps10.5pyapdarren@gmail.com>
2025-12-17 18:15:04 +0800

    edit code.js and index.html and style.css

--- code.js
+++ code.js
@@ -1,10 +1,25 @@
 const grid = document.querySelector(".grid")
 
-for (let i = 0; i < 16; i++){
+let mouseDown = false;
+
+document.body.addEventListener("mousedown", () => mouseDown = true);
+document.body.addEventListener("mouseup", () => mouseDown = false);4
+
+for (let i = 0; i < 30*30; i++){
     const square = document.createElement("div");
 
+
+
+    square.addEventListener("mousedown", paintSquare);
+    square.addEventListener("mouseenter", paintSquare);
+    square.style.flex = `0 0 ${100 / 30}%`;
     square.classList.add("square");
-    square.style.backgroundColor = "orange";
 
     grid.append(square)
+}
+
+function paintSquare(e) {
+    if (e.type === "mousedown" || mouseDown) {
+        this.style.backgroundColor = "black";
+    }
 }
